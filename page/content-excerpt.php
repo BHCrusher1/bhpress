8fdc25e2e11721653d4c1de18f6bfb689738fa2d
BHCrusher1 <blackholecrusher@gmail.com>
2019-07-11 17:52:24 +0900

    コメント

--- page/content-excerpt.php
+++ page/content-excerpt.php
@@ -2,9 +2,9 @@
 <?php get_template_part('page/content-header'); ?>
 <div class="entry-excerpt">
 <?php the_post_thumbnail('thumbnail'); ?>
-<?php if ( has_post_thumbnail() ) : ?>
+<?php if ( has_post_thumbnail() ) : //アイキャッチ画像がある場合 ?>
 	<div class="entry-summary">
-<?php else : ?>
+<?php else : //アイキャッチ画像が無い場合.no-thumbnailを追加 ?>
 	<div class="entry-summary no-thumbnail">
 <?php endif; ?>
 <?php the_excerpt(); ?>
--- page/content.php
+++ page/content.php
@@ -2,15 +2,15 @@
 <?php
 get_template_part('page/content-header');
 get_template_part('page/sns');
-if ( has_post_thumbnail() ) {
+if ( has_post_thumbnail() ) { //アイキャッチ画像がある
 	echo '<div class="single-featured-image-header"><div>';
-	if ( is_single() ) {
+	if ( is_single() ) { //個別投稿のページを表示中
 		the_post_thumbnail( 'large' );
 	} else {
 		the_post_thumbnail();
 	}
 	echo '</div></div><!-- .single-featured-image-header -->';
-} else { echo '<hr>'; }
+} else { echo '<hr>'; } //アイキャッチ画像無し
 ?>
 <div class="entry-content">
 <?php
@@ -21,7 +21,9 @@ if ( has_post_thumbnail() ) {
 <hr>
 <?php get_template_part('page/sns');
 if ( is_single() ) {
-	get_template_part('page/post-navigation');
+	get_template_part('page/post-navigation'); //前後の記事
 	echo '<hr>';
-	get_template_part('page/content-related');} ?>
+	get_template_part('page/content-related'); //同じカテゴリの記事
+}
+?>
 </article>
