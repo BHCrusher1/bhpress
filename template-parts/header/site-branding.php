bff46d5ca7c0db828841fab8d67755dcf128d416
BHCrusher1 <blackholecrusher@gmail.com>
2019-07-16 16:22:20 +0900

    コメント

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -4,8 +4,8 @@
 		<div class="site-logo"><?php the_custom_logo(); ?>
 	</div>
 	<?php endif; ?>
-	<?php $blog_info = get_bloginfo('name'); //サイトのタイトルを取得?>
-	<?php if (! empty($blog_info)) : //サイトのタイトルが存在する場合?>
+	<?php $blog_info = get_bloginfo('name'); //サイトのタイトルを取得
+	if (! empty($blog_info)) : //サイトのタイトルが存在する場合?>
 		<?php if (is_front_page() && is_home()) : //デフォルト設定の最新の投稿を表示になっている場合?>
 			<h1 class="h1 font-weight-bold d-inline site-title"><a class="text-dark"	href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
 		<?php else : ?>
@@ -14,9 +14,8 @@
 	<?php endif; ?>
 
 	<?php
-    $description = get_bloginfo('description', 'display');
-    if ($description || is_customize_preview()) :
-        ?>
+    $description = get_bloginfo('description', 'display'); //サイトのキャッチフレーズを取得
+    if ($description || is_customize_preview()) : //サイトのキャッチフレーズが存在する場合?>
 		<p class="text-secondary d-inline h5 site-description"><?php echo $description; ?></p>
 	<?php endif; ?>
 
