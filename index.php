ffe03757a98fc96208137010865f6f528da93ae0
BHCrusher1 <blackholecrusher@gmail.com>
2019-07-16 18:06:11 +0900

    2019-07-16 18:00時点のものをコミット

--- index.php
+++ index.php
@@ -1,34 +1,35 @@
 <?php get_header(); ?>
-<div class="wrap">
+<div class="container">
     <?php if (is_home()):?>
-    <nav>
-        <!-- 上に表示されるパンくずリスト -->
-        <ol id="breadcrumb" itemprop="Breadcrumb" itemscope="" itemtype="http://data-vocabulary.org/BreadcrumbList">
-            <li itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem"><i
-                    class="fas fa-home"></i><a itemprop="item"
-                    href="<?php esc_url(home_url('/'));?>"
-                    class="home"><span itemprop="name">HOME</span></a>
-                <meta itemprop="position" content="1">
-            </li>
-        </ol>
-    </nav>
+        <nav aria-label="breadcrumb"><!-- 上に表示されるパンくずリスト -->
+            <ol id="breadcrumb" class="breadcrumb my-0" itemprop="Breadcrumb" itemscope="" itemtype="http://data-vocabulary.org/BreadcrumbList">
+                <li class="breadcrumb-item active" aria-current="page" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem"><i class="fas fa-home"></i><a itemprop="item" href="<?php esc_url( home_url( '/' ) );?>" class="home"><span itemprop="name">HOME</span></a><meta itemprop="position" content="1"></li>
+            </ol>
+        </nav>
     <?php elseif (is_page()) : ?>
-    <?php breadcrumb(); ?>
+        <?php breadcrumb(); ?>
     <?php endif;?>
-    <div id="primary">
-        <main>
-            <?php if (have_posts()) :
-    while (have_posts()) : the_post();
-        get_template_part('page/content');
-    endwhile;
-    bhpress_pagination_list();
-else :
-    echo '<p>投稿が見つかりませんでした。</p>';
-endif;
-?>
-        </main>
-    </div><!-- #primary -->
-    <?php get_sidebar(); ?>
-    <!-- サイドバー -->
-</div><!-- .wrap -->
+    <div class="row">
+        <section id="primary" class="col-md-9 content-area">
+            <main id="main" class="site-main">
+                <?php if (have_posts()) {
+                    //コンテンツがあればループ
+                    while (have_posts()) {
+                        the_post();
+                        get_template_part('template-parts/content/content');
+                    }
+
+                    //前後ページのナビゲーション
+                    bhpress_pagination_list();
+                } else {
+
+                    //コンテンツがない場合は"投稿が見つかりません"というテンプレートを含めます
+                    get_template_part('template-parts/content/content', 'none');
+                }
+                ?>
+            </main><!-- .site-main -->
+        </section><!-- .content-area -->
+        <?php /*get_sidebar(); サイドバーを一時的に消す*/ ?> 
+    </div><!-- .row -->
+</div><!-- .container -->
 <?php get_footer();
---
+++ template-parts/content/article-header.php
@@ -0,0 +1,19 @@
+<header class="container bg-primary text-white entry-header">
+<?php
+    if (is_single() or is_page()) { //個別ページの場合
+        the_title('<h1 class="d-inline h2 text-white entry-title">', '</h1>');
+    } elseif (is_front_page() && is_home()) { //トップページ（ホームとフロントページ）の場合
+        the_title('<h3 class="d-inline h2 entry-title"><a class="text-white" href="'. esc_url(get_permalink()) .'" rel="bookmark">', '</a></h3>');
+    } else { //それ以外
+        the_title('<h2 class="d-inline h2 entry-title"><a class="text-white" href="'. esc_url(get_permalink()) .'" rel="bookmark">', '</a></h2>');
+    }
+    if ( ! is_page() ) : //固定ページを表示していない場合 ?>
+        <div class="d-inline entry-meta">
+            <time class="mr-2 post-date" datetime="<?php the_date('c'); ?>">公開日:<?php echo get_the_date(); ?></time>
+            <?php if ( get_the_date() != get_the_modified_date() ) : //公開日と最終更新日が一致しない場合のみ最終更新日を表示?>
+                <time class="mr-2 modified-date" datetime="<?php the_modified_time('c'); ?>">最終更新日:<?php echo the_modified_date(); ?></time>
+            <?php endif; ?>
+            <span class="cat-links"><i class="fas fa-folder"></i>：<?php the_category(', '); ?></span>
+        </div> <!-- .entry-meta -->
+    <?php endif; ?>
+</header>
