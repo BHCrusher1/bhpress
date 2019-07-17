fc3ff59e4cac218b8d3dd1f7965d27e1c47fe7d7
BHCrusher1 <blackholecrusher@gmail.com>
2019-07-17 18:02:37 +0900

    サイドバー仮

--- functions.php
+++ functions.php
@@ -60,8 +60,8 @@ register_sidebar(array(
     'id'            => 'sidebar-1',
     'before_widget' => '<section id="%1$s" class="widget %2$s">',
     'after_widget'  => '</section>',
-    'before_title'  => '<h2 class="widget-title">',
-    'after_title'   => '</h2>',
+    'before_title'  => '<h4 class="container bg-secondary text-white mb-0 widget-title">',
+    'after_title'   => '</h4>',
 ));
 
 function bhpress_pagination_list($args = array())
