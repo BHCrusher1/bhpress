df88e6043884c6b7aed810aeaa5113cdd463bb09
BHCrusher1 <blackholecrusher@gmail.com>
2019-07-18 14:15:57 +0900

    コメント欄

--- comments.php
+++ comments.php
@@ -1,10 +1,15 @@
-<div id="comments" class="comments-area">
+<?php
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<div id="comments" class="container bg-white my-3 <?php echo comments_open() ? 'comments-area' : 'comments-area comments-closed'; ?>">
     <?php if (have_comments()): ?>
-    <h3>コメント</h3>
-    <ol class="commets-list">
+    <h2 class="comments-title">コメント</h3>
+    <ol class="comment-list">
         <?php wp_list_comments('avatar_size=0'); ?>
     </ol>
-    <hr>
     <?php endif; ?>
     <?php comment_form(); ?>
-</div>
+</div><!-- #comments -->
