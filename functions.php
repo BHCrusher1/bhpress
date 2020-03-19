aeaf2a52d1b541ed0d65d484c0fe00644e634d8f
BHCrusher1 <bhcrusher1@bhcrusher1.net>
2020-03-19 12:59:47 +0900

    popperをbundle版にした

--- functions.php
+++ functions.php
@@ -16,8 +16,7 @@ function add_script()
     wp_deregister_script('jquery');
     //スクリプトの読み込み
     wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), null, true);
-    wp_enqueue_script('popper.js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/popper.min.js', array('jquery'), null, true);
-    wp_enqueue_script('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js', array('popper.js'), null, true);
+    wp_enqueue_script('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js', array('jquery'), null, true);
     wp_enqueue_script('script', get_template_directory_uri() . '/addClass.js', array('jquery'), date('YmdHi', filemtime(get_stylesheet_directory() . '/addClass.js')), true);
 }
 add_action('wp_enqueue_scripts', 'add_script');
