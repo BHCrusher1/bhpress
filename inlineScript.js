702bf7460bd3eae9d6324a3af4dc315f83093de3
BHCrusher1 <bhcrusher1@bhcrusher1.net>
2020-06-24 10:00:35 +0900

    スムーズスクロールやクラス追加などのスクリプトをインライン化

---
+++ inlineScript.js
@@ -0,0 +1,25 @@
+jQuery( function( $ ) {
+	$( '.entry-content table' ).addClass( 'table' );
+	$( '.more-link, .read-more' ).addClass( 'btn btn-info my-3' );
+	$( '.cat-links a' ).addClass( 'text-white' );
+	$( '#secondary section' ).addClass( 'mb-3' );
+	$( '#secondary section' ).children( '*' ).not( 'div' ).addClass( 'container' );
+	$( '#secondary section ul' ).addClass( 'list-group list-group-flush p-0' );
+	$( '#secondary section li' ).addClass( 'list-group-item' );
+	$( 'ul.page-numbers' ).addClass( 'pagination mb-0' );
+	$( 'ul.page-numbers li' ).addClass( 'px-3' );
+	$( '#comments .form-submit #submit' ).addClass( 'btn btn-outline-primary' );
+	$( '#comments input[type="text"], #comments input[type="email"], #comments input[type="url"], #comments textarea' ).addClass( 'form-control' );
+
+	// https://qiita.com/Takuya_Kouyama/items/b815eb5e1f85d819b4d8
+	// 要jQuery slim版不可
+	// #で始まるアンカーをクリックした場合に処理
+	$( 'a[href^="#"]' ).click( function() {
+		var speed = 400;
+		var href = $( this ).attr( 'href' );
+		var target = $( href == '#' || href == '' ? 'html' : href );
+		var position = target.offset().top;
+		$( 'body,html' ).animate( { scrollTop: position }, speed, 'swing' );
+		return false;
+	} );
+} );
