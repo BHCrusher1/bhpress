b7915bb6b05aeaba65305672563956c03cd5d450
BHCrusher1 <bhcrusher1@bhcrusher1.net>
2020-07-21 14:53:27 +0900

    投稿のアイキャッチ画像を読み込む部分を別ファイル化

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -7,25 +7,22 @@
 	<?php
 	get_template_part( 'template-parts/article-header' );
 	get_template_part( 'template-parts/sns' );
-	if ( has_post_thumbnail() ) { // アイキャッチ画像がある
-		echo '<div class="single-featured-image-header">';
-		if ( is_single() ) { // 個別投稿のページを表示中
-			the_post_thumbnail( 'large' );
-		} else { // 個別投稿以外のページ
-			the_post_thumbnail();
-		}
-		echo '</div><!-- .single-featured-image-header -->';
-		echo '<div class="container entry-content border-bottom">';
-	} else { // アイキャッチ画像無し
-		echo '<div class="container entry-content border-top pt-3 border-bottom">';
-	}
-	if ( is_single() ) { // 個別投稿のページを表示中
+	get_template_part( 'template-parts/featured-image' );
+	?>
+
+	<div class="container p-3 entry-content">
+
+	<?php
+	// 個別投稿のページを表示中
+	if ( is_single() ) {
 		the_content(); // 全部を表示
 	} else {
 		the_content( '続きを読む ' ); // moreまで表示
 	}
 	?>
+
 	</div><!-- .container entry-content -->
+
 	<?php
 	get_template_part( 'template-parts/sns' );
 	if ( is_single() ) { // 個別投稿のページを表示中
